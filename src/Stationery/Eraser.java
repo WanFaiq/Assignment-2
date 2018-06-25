041d687e6d1bdaa2ff5c01ad76ea203f78937b8d
WanFaiq <39939596+WanFaiq@users.noreply.github.com>
2018-06-26 01:23:06 +0800

    Moved all stationery related classes here

---
+++ src/Stationery/Eraser.java
@@ -0,0 +1,18 @@
+package Stationery;
+
+//Eraser.java
+
+public class Eraser extends Stationeries
+{
+	public Eraser(String stationeryName, String stationeryType, double stationeryPrice)
+	{
+		this.stationeryName=stationeryName;
+		this.stationeryType=stationeryType;
+		this.stationeryPrice=stationeryPrice;
+	}
+	
+	public void print (int i)
+	{
+		System.out.println("Product " + (i + 1) + ": " + stationeryName + "\t-RM" + stationeryPrice);
+	}
+}
